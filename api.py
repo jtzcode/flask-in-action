a9643e160709524331e8545be7ab717b99c0d9ff
Tianze Jiang <tianze.jiang@citrix.com>
2020-08-20 16:15:51 +0800

    Connect to sqlite3 db.

--- api.py
+++ api.py
@@ -1,6 +1,7 @@
 import flask
 import utils
 from flask import request, jsonify
+import sqlite3
 
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
@@ -29,13 +30,62 @@ bookmarks = [
     }
 ]
 
+def dict_facotry(cursor, row):
+    d = {}
+    for idx, col in enumerate(cursor.description):
+        d[col[0]] = row[idx]
+    return d
+
 @app.route('/', methods=['GET'])
 def home():
     return "<h1>Hello Flask</h1><p>This is an API prototype in Python.</p>"
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return "<h1>404 page not found</h1>", 404
+
+
 @app.route('/api/v1/bookmarks/all', methods=['GET'])
 def getAllBookmarks():
     return jsonify(bookmarks)
+
+@app.route('/api/v1/books/all', methods=['GET'])
+def getAllBooks(): 
+    conn = sqlite3.connect('flask-in-action/books.db')
+    conn.row_factory = dict_facotry
+    cur = conn.cursor()
+    allBooks = cur.execute('SELECT * FROM books;').fetchall()
+
+    return jsonify(allBooks)
+
+@app.route('/api/v1/books', methods=['GET'])
+def getBookByFilter():
+    queryStrings = request.args
+    id = queryStrings.get('id')
+    published = queryStrings.get('published')
+    author = queryStrings.get('author')
+
+    query = 'SELECT * FROM books WHERE'
+    toFilter = []
+
+    if id:
+        query += ' id=? AND'
+        toFilter.append(id)
+    if published:
+        query += ' published=? AND'
+        toFilter.append(published)
+    if author:
+        query += ' author=? AND'
+        toFilter.append(author)
+
+    query = query[:-4] + ';'
+
+    conn = sqlite3.connect('flask-in-action/books.db')
+    conn.row_factory = dict_facotry
+    cur = conn.cursor()
+
+    results = cur.execute(query, toFilter).fetchall()
+    return jsonify(results)
 
 @app.route('/api/v1/bookmarks', methods=['GET'])
 def getBookmarkById():
